d7802f4b2f05a6d224c51c4531e7f4d6f9b28066
georgesroberto <georgesroberto21@gmail.com>
2024-05-02 17:22:35 +0300

    Charm

--- frontend/src/views/Loginpage.js
+++ frontend/src/views/Loginpage.js
@@ -1,132 +1,125 @@
-import React, { useContext } from 'react'
-import { Link } from 'react-router-dom'
-import AuthContext from '../context/AuthContext'
-
-
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import "../box.css";
 
 function Loginpage() {
+  const { loginUser } = useContext(AuthContext);
 
-  const {loginUser} = useContext(AuthContext)
-  const handleSubmit = e => {
-    e.preventDefault()
-    const email = e.target.email.value
-    const password = e.target.password.value
+  const [isSignUpActive, setIsSignUpActive] = useState(false);
 
-    email.length > 0 && loginUser(email, password)
+  const handleSignInClick = () => {
+    setIsSignUpActive(false);
+  };
 
-    console.log(email)
-    console.log(password)
-   
-  }
+  const handleSignUpClick = () => {
+    setIsSignUpActive(true);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    email.length > 0 && loginUser(email, password);
+  };
 
   return (
     <div>
       <>
-  <section className="vh-100" style={{ backgroundColor: "#9A616D" }}>
-    <div className="container py-5 h-100">
-      <div className="row d-flex justify-content-center align-items-center h-100">
-        <div className="col col-xl-10">
-          <div className="card" style={{ borderRadius: "1rem" }}>
-            <div className="row g-0">
-              <div className="col-md-6 col-lg-5 d-none d-md-block">
-                <img
-                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                  alt="login form"
-                  className="img-fluid"
-                  style={{ borderRadius: "1rem 0 0 1rem" }}
+        <section className="vh-100">
+          <div className="container py-5 h-100">
+            <div
+              className={`form-container ${
+                isSignUpActive ? "sign-up-container" : ""
+              }`}
+            >
+              <form onSubmit={handleSubmit}>
+                <h1>Sign in</h1>
+                <div className="social-container">
+                  <a href="signup.html?provider=facebook" className="social">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a href="signup.html?provider=google" className="social">
+                    <i className="fab fa-google-plus-g"></i>
+                  </a>
+                  <a href="signup.html?provider=linkedin" className="social">
+                    <i className="fab fa-linkedin-in"></i>
+                  </a>
+                </div>
+                <span>or use your account</span>
+                <input
+                  type="email"
+                  id="form2Example17"
+                  className="form-control form-control-lg"
+                  name="email"
                 />
-              </div>
-              <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                <div className="card-body p-4 p-lg-5 text-black">
-                  <form onSubmit={handleSubmit}>
-                    <div className="d-flex align-items-center mb-3 pb-1">
-                      <i
-                        className="fas fa-cubes fa-2x me-3"
-                        style={{ color: "#ff6219" }}
-                      />
-                      <div className="d-flex align-items-center mb-3 pb-1">
-                        <i
-                          className="fas fa-cubes fa-2x me-3"
-                          style={{ color: "#ff6219" }}
-                        />
-                        <span className="h2 fw-bold mb-0">Welcome back 👋</span>
-                      </div>
-                    </div>
-                    <h5
-                      className="fw-normal mb-3 pb-3"
-                      style={{ letterSpacing: 1 }}
-                    >
-                      Sign into your account
-                    </h5>
-                    <div className="form-outline mb-4">
-                      <input
-                        type="email"
-                        id="form2Example17"
-                        className="form-control form-control-lg"
-                        name='email'
-                      />
-                      <label className="form-label" htmlFor="form2Example17">
-                        Email address
-                      </label>
-                    </div>
-                    <div className="form-outline mb-4">
-                      <input
-                        type="password"
-                        id="form2Example27"
-                        className="form-control form-control-lg"
-                        name='password'
-                      />
-                      <label className="form-label" htmlFor="form2Example27">
-                        Password
-                      </label>
-                    </div>
-                    <div className="pt-1 mb-4">
-                      <button
-                        className="btn btn-dark btn-lg btn-block"
-                        type="submit"
-                      >
-                        Login
-                      </button>
-                    </div>
-                    <a className="small text-muted" href="#!">
-                      Forgot password?
-                    </a>
-                    <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
-                      Don't have an account?{" "}
-                      <Link to="/register" style={{ color: "#393f81" }}>
-                        Register Now 
-                      </Link>
-                    </p>
-                    <a href="#!" className="small text-muted">
-                      Terms of use.
-                    </a>
-                    <a href="#!" className="small text-muted">
-                      Privacy policy
-                    </a>
-                  </form>
+                <input
+                  type="password"
+                  id="form2Example27"
+                  className="form-control form-control-lg"
+                  name="password"
+                />
+                <a href="#!">Forgot your password?</a>
+                <button>Sign In</button>
+              </form>
+            </div>
+
+            <div className="overlay-container">
+              <div className="overlay">
+                <div
+                  className={`overlay-panel overlay-left ${
+                    !isSignUpActive ? "active" : ""
+                  }`}
+                >
+                  <h1>Welcome Back to Premiere Peak! 👋</h1>
+                  <p>
+                    Reconnect with your favorite movies by logging in with your
+                    personal info.
+                  </p>
+                  <Link to="/login" style={{ color: "#393f81" }}>
+                    <button className="ghost" onClick={handleSignInClick}>
+                      Sign In
+                    </button>
+                  </Link>
+                  <br />
+                  <button
+                    className="ghost"
+                    onClick={() => (window.location.href = "pre-loader.html")}
+                  >
+                    Maybe Later
+                  </button>
+                </div>
+                <div
+                  className={`overlay-panel overlay-right ${
+                    isSignUpActive ? "active" : ""
+                  }`}
+                >
+                  <h1>Hello, Movie Buff!</h1>
+                  <p>
+                    Enter your details to embark on an epic cinematic adventure
+                    with us.
+                  </p>
+                  <Link to="/register" style={{ color: "#393f81" }}>
+                    <button className="ghost" onClick={handleSignUpClick}>
+                      Sign Up
+                    </button>
+                  </Link>
+                  <br />
+                  <button
+                    className="ghost"
+                    onClick={() => (window.location.href = "pre-loader.html")}
+                  >
+                    Maybe Later
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </section>
+      </>
     </div>
-  </section>
-  <footer className="bg-light text-center text-lg-start">
-    <div
-      className="text-center p-3"
-      style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-    >
-      © 2019 - till date Copyright:
-      <a className="text-dark" href="https://mdbootstrap.com/">
-        desphixs.com
-      </a>
-    </div>
-  </footer>
-</>
-
-    </div>
-  )
+  );
 }
 
-export default Loginpage
+export default Loginpage;
--- frontend/src/views/Registerpage.js
+++ frontend/src/views/Registerpage.js
@@ -1,152 +1,147 @@
-import {useState, useContext} from 'react'
-import { Link } from 'react-router-dom'
-import AuthContext from '../context/AuthContext'
-
+import { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
+import "../box.css";
 
 function Registerpage() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [password2, setPassword2] = useState("");
 
-  const [email, setEmail] = useState("")
-  const [username, setUsername] = useState("")
-  const [password, setPassword] = useState("")
-  const [password2, setPassword2] = useState("")
+  const { registerUser } = useContext(AuthContext);
 
-  const {registerUser} = useContext(AuthContext)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    registerUser(email, username, password, password2);
+  };
 
-  console.log(email);
-  console.log(username);
-  console.log(password);
-  console.log(password2);
+  const [isSignUpActive, setIsSignUpActive] = useState(true);
 
-
-  const handleSubmit = async e => {
-    e.preventDefault()
-    registerUser(email, username, password, password2)
-  }
-  
+  const handleSignInClick = () => {
+    setIsSignUpActive(false);
+  };
 
   return (
     <div>
       <>
-        <section className="vh-100" style={{ backgroundColor: "#9A616D" }}>
+        <section className="vh-100">
           <div className="container py-5 h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-              <div className="col col-xl-10">
-                <div className="card" style={{ borderRadius: "1rem" }}>
-                  <div className="row g-0">
-                    <div className="col-md-6 col-lg-5 d-none d-md-block">
-                      <img
-                        src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                        alt="login form"
-                        className="img-fluid"
-                        style={{ borderRadius: "1rem 0 0 1rem" }}
-                      />
-                    </div>
-                    <div className="col-md-6 col-lg-7 d-flex align-items-center">
-                      <div className="card-body p-4 p-lg-5 text-black">
-                        <form onSubmit={handleSubmit}>
-                          <div className="d-flex align-items-center mb-3 pb-1">
-                            <i
-                              className="fas fa-cubes fa-2x me-3"
-                              style={{ color: "#ff6219" }}
-                            />
-                            <span className="h2 fw-bold mb-0">
-                              Welcome to <b>Desphixs👋</b>
-                            </span>
-                          </div>
-                          <h5
-                            className="fw-normal mb-3 pb-3"
-                            style={{ letterSpacing: 1 }}
-                          >
-                            Sign Up
-                          </h5>
-                          <div className="form-outline mb-4">
-                            <input
-                              type="email"
-                              id="form2Example17"
-                              className="form-control form-control-lg"
-                              placeholder="Email Address"
-                              onChange={e => setEmail(e.target.value)}
-                            />
-                          </div>
-                          <div className="form-outline mb-4">
-                            <input
-                              type="text"
-                              id="form2Example17"
-                              className="form-control form-control-lg"
-                              placeholder="Username"
-                              onChange={e => setUsername(e.target.value)}
+            <div
+              className={`form-container ${
+                isSignUpActive ? "sign-up-container" : "right-panel-active"
+              }`}
+            >
+              <form
+                onSubmit={handleSubmit}
+                style={{ backgroundColor: "#9A616D" }}
+              >
+                <h1>Create Account</h1>
+                <div className="social-container">
+                  <a href="/" className="social">
+                    <i className="fab fa-facebook-f"></i>
+                  </a>
+                  <a href="/" className="social">
+                    <i className="fab fa-google-plus-g"></i>
+                  </a>
+                  <a href="/" className="social">
+                    <i className="fab fa-linkedin-in"></i>
+                  </a>
+                </div>
+                <span>or use your email for registration</span>
+                <span style={{ Color: "#00000" }}>Username</span>
+                <input
+                  type="text"
+                  id="form2Example17"
+                  className="form-control"
+                  placeholder="Username"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <span style={{ Color: "#00000" }}>Email</span>
+                <input
+                  type="email"
+                  id="form2Example17"
+                  className="form-control"
+                  placeholder="Email Address"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <span style={{ Color: "#00000" }}>Passweord</span>
+                <input
+                  type="password"
+                  id="form2Example17"
+                  className="form-control"
+                  placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <span style={{ Color: "#00000" }}>Password</span>
+                <input
+                  type="password"
+                  id="form2Example27"
+                  className="form-control"
+                  placeholder="Confirm Password"
+                  onChange={(e) => setPassword2(e.target.value)}
+                />
+                <button>Sign Up</button>
+              </form>
+            </div>
 
-                            />
-                          </div>
-                          <div className="form-outline mb-4">
-                            <input
-                              type="password"
-                              id="form2Example17"
-                              className="form-control form-control-lg"
-                              placeholder="Password"
-                              onChange={e => setPassword(e.target.value)}
+            <div className="overlay-container">
+              <div className="overlay">
+                <div
+                  className={`overlay-panel overlay-left right-panel-active ${
+                    !isSignUpActive ? "active" : ""
+                  }`}
+                >
+                  <h1>Welcome Back to Premiere Peak!</h1>
+                  <p>
+                    Reconnect with your favorite movies by logging in with your
+                    personal info.
+                  </p>
 
-                            />
-                          </div>
-                          <div className="form-outline mb-4">
-                            <input
-                              type="password"
-                              id="form2Example27"
-                              className="form-control form-control-lg"
-                              placeholder="Confirm Password"
-                              onChange={e => setPassword2(e.target.value)}
-
-                            />
-                          </div>
-                          <div className="pt-1 mb-4">
-                            <button
-                              className="btn btn-dark btn-lg btn-block"
-                              type="submit"
-                            >
-                              Register
-                            </button>
-                          </div>
-                          <a className="small text-muted" href="#!">
-                            Forgot password?
-                          </a>
-                          <p className="mb-5 pb-lg-2" style={{ color: "#393f81" }}>
-                            Already have an account?{" "}
-                            <Link to="/login" style={{ color: "#393f81" }}>
-                              Login Now
-                            </Link>
-                          </p>
-                          <a href="#!" className="small text-muted">
-                            Terms of use.
-                          </a>
-                          <a href="#!" className="small text-muted">
-                            Privacy policy
-                          </a>
-                        </form>
-                      </div>
-                    </div>
-                  </div>
+                  <Link to="/login" style={{ color: "#393f81" }}>
+                    <button className="ghost" onClick={handleSignInClick}>
+                      Sign In
+                    </button>
+                  </Link>
+                  <br />
+                  <button
+                    className="ghost"
+                    onClick={() => (window.location.href = "pre-loader.html")}
+                  >
+                    Maybe Later
+                  </button>
+                </div>
+                <div
+                  className={`overlay-panel overlay-right right-panel-active ${
+                    isSignUpActive ? "active" : ""
+                  }`}
+                >
+                  <h1>Hello, Movie Buff!</h1>
+                  <p>
+                    Enter your details to embark on an epic cinematic adventure
+                    with us.
+                  </p>
+                  <Link to="/register" style={{ color: "#393f81" }}>
+                    <button className="ghost" id="signUp">
+                      Sign Up
+                    </button>
+                  </Link>
+                  <br />
+                  <button
+                    className="ghost"
+                    id=""
+                    onClick={() => (window.location.href = "pre-loader.html")}
+                  >
+                    Maybe Later
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </section>
-        <footer className="bg-light text-center text-lg-start">
-          {/* Copyright */}
-          <div
-            className="text-center p-3"
-            style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
-          >
-            © 2019 - till date Copyright:
-            <a className="text-dark" href="https://mdbootstrap.com/">
-              desphixs.com
-            </a>
-          </div>
-          {/* Copyright */}
-        </footer>
-    </>
-
+      </>
     </div>
-  )
+  );
 }
 
-export default Registerpage
+export default Registerpage;
